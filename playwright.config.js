783866e1f758d9e801dffd03bea9d3c00b1bfe38
manju <cskmnrpt@gmail.com>
2024-06-21 23:37:27 +0530

    adding config, package.json and tests

---
+++ playwright.config.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const config = {
+  use: {
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  reporter: [
+    ['list'],
+    [
+      'playwright-qase-reporter',
+      {
+        debug: process.env.QASE_DEBUG,
+        testops: {
+          api: {
+            token: process.env.QASE_TESTOPS_API_TOKEN,
+          },
+          project: process.env.QASE_TESTOPS_PROJECT,
+          uploadAttachments: true,
+          run: {
+            complete: process.env.QASE_TESTOPS_RUN_COMPLETE,
+          },
+        },
+      },
+    ],
+  ],
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+};
+
+module.exports = config;
